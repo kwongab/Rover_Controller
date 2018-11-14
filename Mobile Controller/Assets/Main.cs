d57c71726e0f8b01cec281931f3cf10d67656d0d
kwongab <kwongab@uw.edu>
2018-11-14 15:32:11 -0800

    first commit

---
+++ Mobile Controller/Assets/Main.cs
@@ -0,0 +1,105 @@
+﻿using Scarlet.Communications;
+using Scarlet.Utilities;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Main : MonoBehaviour {
+
+    public Joystick left;
+    public Joystick right;
+    public Joystick all;
+    public Button toSingleButton;
+    public Button toDoubleButton;
+    public bool debug;
+    public GameObject connected;
+    public GameObject disconnected;
+    public int PERIOD;
+
+    private bool useSingleJoystick;
+    private float horizontal;
+    private float vertical;
+    private int count;
+
+	// Use this for initialization
+	void Start () {
+        toSingleButton.onClick.AddListener(() => switchToSingle());
+        toDoubleButton.onClick.AddListener(() => switchToDouble());
+        horizontal = 0;
+        vertical = 0;
+        useSingleJoystick = false;
+        count = 0;
+        Scarlet.Communications.Client.Start("192.168.0.25", 1025, 1026, "Mobile", OperationPeriod: PERIOD);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Client.IsConnected)
+        {
+            connected.SetActive(true);
+            disconnected.SetActive(false);
+        }
+        else
+        {
+            connected.SetActive(false);
+            disconnected.SetActive(true);
+        }
+
+
+        float skidSteerSpeed;
+        float skidDriveSpeed;
+
+        if (!useSingleJoystick)
+        {
+            skidSteerSpeed = left.Horizontal;
+            skidDriveSpeed = right.Vertical;
+        }
+        else
+        {
+            skidSteerSpeed = all.Horizontal;
+            skidDriveSpeed = all.Vertical;
+        }
+
+
+        if (debug)
+        {
+            Debug.Log("turn = " + skidSteerSpeed);
+            Debug.Log("drive = " + skidDriveSpeed);
+        }
+        count++;
+        if (count == 5)
+        {
+
+            Packet SkidFrontRight = new Packet(0x90, true, "MainRover");
+            SkidFrontRight.AppendData(UtilData.ToBytes(((skidDriveSpeed - skidSteerSpeed) * 2)));
+            Scarlet.Communications.Client.Send(SkidFrontRight);
+
+            Packet SkidRearRight = new Packet(0x92, true, "MainRover");
+            SkidRearRight.AppendData(UtilData.ToBytes(((skidDriveSpeed - skidSteerSpeed) * 2)));
+            Scarlet.Communications.Client.Send(SkidRearRight);
+
+            Packet SkidFrontLeft = new Packet(0x91, true, "MainRover");
+            SkidFrontLeft.AppendData(UtilData.ToBytes(((skidDriveSpeed + skidSteerSpeed) * 2)));
+            Scarlet.Communications.Client.Send(SkidFrontLeft);
+
+            Packet SkidRearLeft = new Packet(0x93, true, "MainRover");
+            SkidRearLeft.AppendData(UtilData.ToBytes((0 - skidDriveSpeed - skidSteerSpeed) * 2));
+            //Console.WriteLine("Test " + (-skidDriveSpeed - skidSteerSpeed));
+            Scarlet.Communications.Client.Send(SkidRearLeft);
+            count = 0;
+        }
+    }
+
+
+    public void switchToSingle()
+    {
+        useSingleJoystick = true;
+    }
+
+    public void switchToDouble()
+    {
+        useSingleJoystick = false;
+    }
+}
---
+++ Relay/TempMainRover/Program.cs
@@ -0,0 +1,116 @@
+using System;
+using Scarlet;
+using Scarlet.IO.BeagleBone;
+using System.Collections.Generic;
+using Scarlet.Components;
+using Scarlet.Components.Sensors;
+using Scarlet.Communications;
+using Scarlet.Utilities;
+using System.Threading;
+using System.IO.Ports;
+using System.Linq;
+
+namespace MainRover
+{
+    public class MainRover
+    {
+        public static string CLIENT_IP = "192.168.0.5";
+
+        public static bool Quit;
+        public static List<ISensor> Sensors;
+        public static QueueBuffer Packets;
+        public static SerialPort Port;
+        private static string SerialPortName;
+        public static int count;
+                
+
+        public static void SetupClient()
+        {
+            count = 0;
+            ///Client.Start(CLIENT_IP, 1025, 1026, "MainRover", OperationPeriod: 5);
+            Server.Start(1025, 1026, OperationPeriod: 5);
+            Packets = new QueueBuffer();
+            for (byte i = 0x8E; i <= 0x99; i++)
+                Parse.SetParseHandler(i, (Packet) => Packets.Enqueue(Packet, 0));
+        }
+
+        public static void ProcessInstructions()
+        {
+            const int NUM_PACKETS_TO_PROCESS = 20;
+            for (int i = 0; !Packets.IsEmpty() && i < NUM_PACKETS_TO_PROCESS; i++)
+            {
+                Packet p = Packets.Dequeue();
+                count++;
+                
+                switch ((PacketID)p.Data.ID)
+                {
+                    case PacketID.RPMAllDriveMotors:
+                        //MotorControl.SetAllRPM((sbyte)p.Data.Payload[0]);
+                        break;
+                    case PacketID.RPMFrontRight:
+                    case PacketID.RPMFrontLeft:
+                    case PacketID.RPMBackRight:
+                    case PacketID.RPMBackLeft:
+                        int MotorID = p.Data.ID - (byte)PacketID.RPMFrontRight;
+                        
+                        /*
+                        int sign = 1;
+                        if ((sbyte) p.Data.Payload[0] < 0)
+                        {
+                            sign = -1;
+                        }
+                        Console.WriteLine("motor Id" + MotorID + "data is " + sign * (sbyte)p.Data.Payload[0]);      */  
+                        if (count == 5)
+                        {
+                            Console.WriteLine("motor Id" + MotorID + "data is " + (sbyte)p.Data.Payload[0]);
+                            Packet output = new Packet(p.Data.ID, true, "MainRover");
+                            output.AppendData(UtilData.ToBytes(translate((sbyte)p.Data.Payload[0])));
+                            Scarlet.Communications.Server.Send(output);
+                            count = 0;
+                        } 
+                        
+                        break;
+                    case PacketID.RPMSteeringMotor:
+                        float SteerSpeed = UtilData.ToFloat(p.Data.Payload);
+                        //MotorControl.SetSteerSpeed(SteerSpeed);
+                        //TODO: Figure out what this was supposed to do.
+                        break;
+                    case PacketID.SteerPosition:
+                        float Position = UtilData.ToFloat(p.Data.Payload);
+                        //MotorControl.SetRackAndPinionPosition(Position);
+                        
+                        break;
+                    
+                        
+                } 
+            }
+        }
+
+        public static float translate (sbyte load)
+        {
+            if (load > 0)
+            {
+                return 0.9f;
+            }
+            else if (load < 0)
+            {
+                return -0.9f;
+            }
+            return 0f;
+        }
+
+        public static void Main(string[] args)
+        {            
+            ///Console.Write("Enter phone IP adress: ");
+            ///String input = Console.ReadLine();
+            ///CLIENT_IP = input;
+            Quit = false;
+            SetupClient();
+            do
+            {
+                ProcessInstructions();
+                Thread.Sleep(50);
+            } while (!Quit);
+        }
+    }
+}
