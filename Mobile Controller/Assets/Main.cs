7208f8650cf071c4e9fa344c1a2bdd3449f2c61b
kwongab <kwongab@uw.edu>
2018-11-16 19:13:00 -0800

    published app

--- Mobile Controller/Assets/Main.cs
+++ Mobile Controller/Assets/Main.cs
@@ -73,21 +73,21 @@ public class Main : MonoBehaviour {
         {
 
             Packet SkidFrontRight = new Packet(0x90, true, "MainRover");
-            SkidFrontRight.AppendData(UtilData.ToBytes((sbyte)Math.Round((skidDriveSpeed - skidSteerSpeed) * 120)));
+            SkidFrontRight.AppendData(UtilData.ToBytes((sbyte)Math.Round((skidDriveSpeed - skidSteerSpeed) * 60)));
             //SkidFrontRight.AppendData(UtilData.ToBytes((sbyte)Math.Round((skidDriveSpeed) * 120)));
             //Debug.Log((sbyte)SkidFrontRight.Data.Payload[0] + " " + SkidFrontRight.Data.Payload[1]);
             Scarlet.Communications.Client.Send(SkidFrontRight);
 
             Packet SkidRearRight = new Packet(0x92, true, "MainRover");
-            SkidRearRight.AppendData(UtilData.ToBytes((sbyte)Math.Round((skidDriveSpeed - skidSteerSpeed) * 120)));
+            SkidRearRight.AppendData(UtilData.ToBytes((sbyte)Math.Round((skidDriveSpeed - skidSteerSpeed) * 60)));
             Scarlet.Communications.Client.Send(SkidRearRight);
 
             Packet SkidFrontLeft = new Packet(0x91, true, "MainRover");
-            SkidFrontLeft.AppendData(UtilData.ToBytes((sbyte)Math.Round((skidDriveSpeed + skidSteerSpeed) * 120)));
+            SkidFrontLeft.AppendData(UtilData.ToBytes((sbyte)Math.Round((skidDriveSpeed + skidSteerSpeed) * 60)));
             Scarlet.Communications.Client.Send(SkidFrontLeft);
 
             Packet SkidRearLeft = new Packet(0x93, true, "MainRover");
-            SkidRearLeft.AppendData(UtilData.ToBytes((sbyte)Math.Round((0 - skidDriveSpeed - skidSteerSpeed) * 120)));
+            SkidRearLeft.AppendData(UtilData.ToBytes((sbyte)Math.Round((0 - skidDriveSpeed - skidSteerSpeed) * 60)));
             //Console.WriteLine("Test " + (-skidDriveSpeed - skidSteerSpeed));
             Scarlet.Communications.Client.Send(SkidRearLeft);
             count = 0;
